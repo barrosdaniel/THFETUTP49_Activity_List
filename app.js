cf36d2fe0df74bdd4f1e2783c857627be28dda45
barros.daniel@outlook.com <barros.daniel@outlook.com>
2019-07-18 21:46:03 +0800

    feature: Add Down button to list items

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Variable Selectors
 const toggleList = document.getElementById('toggleList');
 const listDiv = document.querySelector('.list');
 const descriptionInput = document.querySelector('input.description');
@@ -7,25 +8,34 @@ const listUl = listDiv.querySelector('ul');
 const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
 
-// console.log(listDiv);
-
+// List Item Action Buttons
 listUl.addEventListener('click', (event) => {
   if (event.target.tagName === 'BUTTON') {
     if (event.target.className === 'remove') {
+      // Remove button
       let li = event.target.parentNode;
       let ul = li.parentNode;
       ul.removeChild(li);
     } else if (event.target.className === 'up') {
+      // Up button
       let li = event.target.parentNode;
       let prevLi = li.previousElementSibling;
       let ul = li.parentNode;
       if (prevLi) {
         ul.insertBefore(li, prevLi);
       }
+    } else if (event.target.className === 'down') {
+      let li = event.target.parentNode;
+      let nextLi = li.nextElementSibling;
+      let ul = li.parentNode;
+      if (nextLi) {
+        ul.insertBefore(nextLi, li);
+      }
     }
   }
 });
 
+// Hide List Button
 toggleList.addEventListener('click', () => {
   if (listDiv.style.display === 'none') {
     toggleList.textContent = 'Hide List';
@@ -36,15 +46,17 @@ toggleList.addEventListener('click', () => {
   }
 })
 
+// List Description Button
 descriptionButton.addEventListener('click', () => {
   descriptionP.innerHTML = descriptionInput.value + ':';
   descriptionInput.value = '';
 })
 
+// Remove Button
 addItemButton.addEventListener('click', () => {
   const ul = document.querySelectorAll('.list ul')[0];
   let li = document.createElement('li');
-  li.innerHTML = addItemInput.value + '<button class="up">Up</button><button class="remove">Remove</button>';
+  li.innerHTML = `${addItemInput.value} <button class="up">Up</button><button class="down">Down</button><button class="remove">Remove</button>`;
   ul.appendChild(li);
   addItemInput.value = "";
 })
